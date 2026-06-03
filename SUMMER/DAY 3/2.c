084c4440aabc41da031f06ef579878aba9254405
MAYANK SRIVASTAVA <mayanksrivastav5612@gmail.com>
2026-06-04 00:09:01 +0530

    Add files via upload

---
+++ SUMMER/DAY 3/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int start, end;
+    printf("Enter start of range: ");
+    scanf("%d", &start);
+    printf("Enter end of range: ");
+    scanf("%d", &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
